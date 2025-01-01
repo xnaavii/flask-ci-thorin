2e5d1032bfe14cf70192f479fecff57f001e535c
Ivan <petrovic.valpovo@gmail.com>
2025-01-01 21:52:14 +0000

    add startboostrap template

--- run.py
+++ run.py
@@ -8,17 +8,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5550")),
-        debug=True,
+        debug=False,
     )
